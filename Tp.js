c5b1647bbfb59725a7608c53c7ecaedb18dd5805
falilou14 <71135356+falilou14@users.noreply.github.com>
2023-02-28 15:43:23 +0100

    modifications

--- Tp.js
+++ Tp.js
@@ -1,31 +1,35 @@
-const fs = require('fs'); 
-let rawdata = fs.readFileSync('users.json');
+const fs = require("fs"); 
+let rawdata = fs.readFileSync("users.json");
 
 let user = JSON.parse(rawdata);
 
 console.log(user); // 1er test d'affichage du fichier Users
 
 
-
+/**
+ * Fonction qui affiche les pays et leur compteur en ordre decroissant
+ */
 function printCountry(){ 
-    let res = {}
-    let tab = []
-    for (i=0; i<user.length ;i++){
+    
+    let res = {};
+    let tab = [];
+    
+    for (let i=0; i<user.length ;i++){
         var CountryName = user[i].country;
         //console.log(CountryName);
         if (!res[CountryName]) { 
-            res[CountryName] = 1
+            res[CountryName] = 1;
         }
         else
-            res[CountryName]++
+            res[CountryName]++ ;
     }
 
-   // console.log(res)
+    // console.log(res)
     for (let i in res) {
-        tab.push({ "country": i, "count": res[i] })
+        tab.push({ "country": i, "count": res[i] });
     }
     tab.sort((a, b) => b.count - a.count);
-    console.log(tab)
+    console.log(tab);
 }
 
 printCountry();
