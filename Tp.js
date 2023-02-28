2f81763876405179cdcb247142f3dbf5f02ed8b1
falilou14 <71135356+falilou14@users.noreply.github.com>
2023-02-28 15:30:51 +0100

    modif

--- Tp.js
+++ Tp.js
@@ -1,6 +1,31 @@
 const fs = require('fs'); 
-let rawdata = fs.readFileSync('student.json');
+let rawdata = fs.readFileSync('users.json');
 
 let user = JSON.parse(rawdata);
 
-console.log(user);
+console.log(user); // 1er test d'affichage du fichier Users
+
+
+
+function printCountry(){ 
+    let res = {}
+    let tab = []
+    for (i=0; i<user.length ;i++){
+        var CountryName = user[i].country;
+        //console.log(CountryName);
+        if (!res[CountryName]) { 
+            res[CountryName] = 1
+        }
+        else
+            res[CountryName]++
+    }
+
+   // console.log(res)
+    for (let i in res) {
+        tab.push({ "country": i, "count": res[i] })
+    }
+    tab.sort((a, b) => b.count - a.count);
+    console.log(tab)
+}
+
+printCountry();
