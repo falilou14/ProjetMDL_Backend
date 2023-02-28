2728dcc425132d08a0009309aa47f2cb158e2322
falilou14 <71135356+falilou14@users.noreply.github.com>
2023-02-28 20:24:01 +0100

    Ajout de la 2em méthode

--- Tp.js
+++ Tp.js
@@ -11,25 +11,54 @@ console.log(user); // 1er test d'affichage du fichier Users
  */
 function printCountry(){ 
     
-    let res = {};
+    let resultat = {}; 
     let tab = [];
     
     for (let i=0; i<user.length ;i++){
         var CountryName = user[i].country;
         //console.log(CountryName);
-        if (!res[CountryName]) { 
-            res[CountryName] = 1;
+        if (!resultat[CountryName]) { 
+            resultat[CountryName] = 1;
         }
         else
-            res[CountryName]++ ;
+            resultat[CountryName]++ ;
     }
   
-    // console.log(res)
-    for (let i in res) {
-        tab.push({ "country": i, "count": res[i] });
+    // console.log(resultat)
+    for (let i in resultat) {
+        tab.push({ "country": i, "count": resultat[i] });
     }
-    tab.sort((a, b) => b.count - a.count);
+    tab.sort((a, b) => b.count - a.count); // on trie de façon décroissante 
     console.log(tab);
 }
 
 printCountry();
+
+
+/**
+ * Fonction qui affiche les compagies et leur compteur en ordre decroissant
+ */
+function printCompany(){
+    let sortie = {}; 
+    let tableau = [];
+
+    for (let i=0; i<user.length ;i++){
+        var CompanyName = user[i].company;
+        
+        if (!sortie[CompanyName]) { 
+            sortie[CompanyName] = 1;
+        }
+        else
+            sortie[CompanyName]++ ;
+    }
+  
+    // console.log(resultat)
+    for (let i in sortie) {
+        tableau.push({ "company": i, "count": sortie[i] });
+    }
+    tableau.sort((a, b) => b.count - a.count); // on trie de façon décroissante 
+    console.log(tableau);
+
+}
+
+printCompany();
