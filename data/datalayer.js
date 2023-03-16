af9be84b6b01570ec148cc477ecc6f75e743ca86
falilou14 <71135356+falilou14@users.noreply.github.com>
2023-03-16 14:59:45 +0100

    "ajout de fichiers"

---
+++ data/datalayer.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+// const proc = require("process");
+
+// require("dotenv").config();
+const filename = "./data/customers.json";
+
+let datalayer = {
+    getAllCustomers: function () {
+        //read json file
+        const data = fs.readFileSync(filename);
+
+        //parse to object
+        const customers = JSON.parse(data);
+
+        //return customers
+        return customers;
+    },
+
+    getCustomers: function (number, page) {
+        {
+
+
+            //read json file
+            let rawdata = fs.readFileSync(filename);
+
+            //parse to object
+            let customers = JSON.parse(rawdata);
+
+            const total = customers.length;
+
+            //filter by number and page
+            if (number && page) {
+                customers = customers.slice((page - 1) * number, page)
+            }
+
+            const result = {
+                total: total,
+                result: customers
+            };
+
+            return result;
+
+        }
+    }
+};
+
+module.exports = datalayer;
